4d6907feef3b2d1afa01ef8daade8d836ab48740
cathiecode <cathiecode@gmail.com>
2026-06-22 13:06:05 +0900

    add: footanchor.cs

---
+++ Basis/Packages/com.superneko.basis.sleepkit/FootAnchor.cs
@@ -0,0 +1,10 @@
+using Basis.Scripts.BasisSdk.Players;
+using Org.BouncyCastle.Bcpg;
+using UnityEditor;
+using UnityEngine;
+
+public class BasisFootAnchor
+{
+    public const string RightFoot = "RightFoot";
+    public const string LeftFoot = "LeftFoot";
+}
